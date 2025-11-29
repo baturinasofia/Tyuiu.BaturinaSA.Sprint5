4f3e506fc3d5d9b16dcc6c969809a2a9deec7b55
baturinasofia <sofi1608@bk.ru>
2025-11-29 16:39:54 +0500

    Task1

--- Tyuiu.BaturinaSA.Sprint5.Task1.V1.Test/DataServiceTest.cs
+++ Tyuiu.BaturinaSA.Sprint5.Task1.V1.Test/DataServiceTest.cs
@@ -9,12 +9,11 @@ namespace Tyuiu.BaturinaSA.Sprint5.Task1.V1.Test
         [TestMethod]
         public void TestMethod1()
         {
-            string path = @"C:\Users\Софья\source\repos\Tyuiu.BaturinaSA.Sprint5\Tyuiu.BaturinaSA.Sprint5.Task1.V1\bin\Debug\OutPutFileTask0.txt";
+            string path = @"C:\Users\kmert\AppData\Local\Temp\OutPutFileTask1";
 
             FileInfo fileInfo = new FileInfo(path);
             bool fileExists = fileInfo.Exists;
-            bool wait = true;
-
+            bool wait = false;
             Assert.AreEqual(wait, fileExists);
         }
     }
