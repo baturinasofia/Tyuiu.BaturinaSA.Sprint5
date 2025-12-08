15f74e5677b9478e6d72245dd9b93aeab63fa862
baturinasofia <sofi1608@bk.ru>
2025-12-08 23:28:24 +0500

    Task4

---
+++ Tyuiu.BaturinaSA.Sprint5.Task4.V15.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.BaturinaSA.Sprint5.Task4.V15.Lib
+{
+    public class DataService : ISprint5Task4V15
+    {
+        public double LoadFromDataFile(string path)
+        {
+            string strX = File.ReadAllText(path);
+            strX = strX.Trim().Replace(".", ",");
+            //double strX1 = Convert.ToDouble(strX);
+            double res = Math.Sin(Convert.ToDouble(strX)) + (Math.Pow(Convert.ToDouble(strX), 2) / 2);
+            return Math.Round(res, 3);
+        }
+    }
+}
---
+++ Tyuiu.BaturinaSA.Sprint5.Task4.V15/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.BaturinaSA.Sprint5.Task4.V15.Lib;
+
+namespace Tyuiu.BaturinaSA.Sprint5.Task4.V15;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #5 | Выполнила: Батурина С. А. | ИСТНб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Чтение данных из текстового файла                                 *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #15                                                             *");
+        Console.WriteLine("* Выполнила: Батурина Софья Алексеевна | ИСТНб-25-1                       *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Прочитать значение из файла и подставить вместо Х в формуле             *");
+        Console.WriteLine("* Вычислить значение по формуле и вернуть полученный результат на консоль *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        string path = $@"{Directory.GetCurrentDirectory()}\InPutDataFileTask4V15.txt";
+        Console.WriteLine("Данные находятся в файле:" + path);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        double res = ds.LoadFromDataFile(path);
+
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }
+}
