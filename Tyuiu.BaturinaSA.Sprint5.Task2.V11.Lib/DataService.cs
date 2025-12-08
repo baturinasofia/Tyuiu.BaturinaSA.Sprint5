feade93278eb08b163098ffd7a735b92d528f7f1
baturinasofia <sofi1608@bk.ru>
2025-12-08 22:44:30 +0500

    Task2

---
+++ Tyuiu.BaturinaSA.Sprint5.Task2.V11.Lib/DataService.cs
@@ -0,0 +1,63 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.BaturinaSA.Sprint5.Task2.V11.Lib
+{
+    public class DataService : ISprint5Task2V11
+    {
+        public string SaveToFileTextData(int[,] matrix)
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask2.txt");
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExsists = fileInfo.Exists;
+
+            if (fileExsists)
+            {
+                File.Delete(path);
+            }
+
+            int rows = matrix.GetLength(0);
+            int columns = matrix.GetLength(1);
+            //int[,] res = new int[rows, columns];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (matrix[i, j] % 2 != 0)
+                    {
+                        matrix[i, j] = 0;
+                    }
+                }
+            }
+
+            string str = "";
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    if (j != columns - 1)
+                    {
+                        str = str + matrix[i, j] + ";";
+                    }
+                    else
+                    {
+                        str = str + matrix[i, j];
+                    }
+                }
+                if (i != rows - 1)
+                {
+                    File.AppendAllText(path, str + Environment.NewLine);
+                }
+                else
+                {
+                    File.AppendAllText(path, str);
+                }
+
+                str = "";
+            }
+            return path;
+
+        }
+    }
+}
---
+++ Tyuiu.BaturinaSA.Sprint5.Task2.V11/Program.cs
@@ -0,0 +1,56 @@
+﻿using Tyuiu.BaturinaSA.Sprint5.Task2.V11.Lib;
+namespace Tyuiu.BaturinaSA.Sprint5.Task2.V11;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] mtrx = new int[3, 3] { { 4, 9, 3 }, { 5, 8, 8 }, { 5, 7, 5 } };
+
+        int rows = mtrx.GetUpperBound(0) + 1;
+        int columns = mtrx.Length / rows;
+
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #5 | Выполнила: Батурина С . А. | ИСТНб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Класс File. Запись структурированных данных в текстовый файл      *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #11                                                             *");
+        Console.WriteLine("* Выполнила: Батурина Софья Алексеевна | ИСТНб-25-1                       *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дан двумерный целочисленный массив 3 на 3 элементов                     *");
+        Console.WriteLine("* Заменить нечетные элементы массива на 0.                                *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.WriteLine($"{mtrx[i, j]} \t");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+
+        string res = ds.SaveToFileTextData(mtrx);
+
+
+        string resultFromFile = File.ReadAllText(res);
+
+        Console.WriteLine("Результат:" + resultFromFile);
+        Console.WriteLine("Файл: " + res);
+        Console.WriteLine("Создан!");
+        Console.ReadKey();
+    }
+}
