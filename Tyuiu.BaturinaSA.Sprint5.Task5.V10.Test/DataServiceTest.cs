0e0065f80a7c50759738541a7f56657bdf3ab742
baturinasofia <sofi1608@bk.ru>
2025-12-09 22:26:10 +0500

    Task5

---
+++ Tyuiu.BaturinaSA.Sprint5.Task5.V10.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.BaturinaSA.Sprint5.Task5.V10.Lib;
+
+namespace Tyuiu.BaturinaSA.Sprint5.Task5.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\0\AppData\Local\Temp\InPutDataFileTask5V10.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = false;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.BaturinaSA.Sprint5.Task5.V10/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.BaturinaSA.Sprint5.Task5.V10.Lib;
+namespace Tyuiu.BaturinaSA.Sprint5.Task5.V10;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #5 | Выполнила: Батурина С. А. | ИСТНб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Чтение набора данных из текстового файла                          *");
+        Console.WriteLine("* Задание #5                                                              *");
+        Console.WriteLine("* Вариант #10                                                             *");
+        Console.WriteLine("* Выполнила: Батурина Софья Алексеевна | ИСТНб-25-1                       *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Найти сумму всех четных целых чисел в файле.                            *");
+        Console.WriteLine("* У вещественных значений округлить до трёх знаков после запятой.         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        string path = @"C:\Users\Софья\source\repos\Tyuiu.BaturinaSA.Sprint5\Tyuiu.BaturinaSA.Sprint5.Task5.V10\bin\Debug\net8.0\InPutDataFileTask5V10.txt";
+
+        Console.WriteLine("Данные находятся в файле: " + path);
+        Console.WriteLine();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        double res = ds.LoadFromDataFile(path);
+
+        Console.WriteLine("Сумма элементов файла: " + res);
+        Console.ReadKey();
+    }
+}
